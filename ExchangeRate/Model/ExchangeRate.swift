3c9841e7b7dfa66378df967099acad00e0edc64d
ChangMin <sopg04@gmail.com>
2022-11-19 17:29:31 +0900

    폴더링 작업

---
+++ ExchangeRate/Model/ExchangeRate.swift
@@ -0,0 +1,15 @@
+//
+//  ExchangeRate.swift
+//  ExchangeRate
+//
+//  Created by ChangMin on 2022/11/19.
+//
+
+import Foundation
+
+struct ExchangeRate {
+    let name: String
+    let unit: String
+    let buying: String
+    let selling: String
+}
